8ed0957e81317e75e44647c8293ef20b3e1e5ded
Gabriel Livinalli <131381519+GLivinalli@users.noreply.github.com>
2025-02-25 03:58:24 +0000

    att

--- xadrez.c
+++ xadrez.c
@@ -5,63 +5,26 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    int qnt_mov, escolha_peca, i;
-    char mov_pc[50];
-    char* peca;
-
-    printf("Escolha a peça!\nBispo.\nTorre.\nRainha.\n");
-    scanf("%d", &escolha_peca);
-
-    switch (escolha_peca)
-    {
-    case 1:
-        peca = "Bispo";
-        printf("Escolha qual direção do Bispo!\n1-Diagonal,Esquerda\n2-Diagonal,Direita\n\n");
-        scanf("%s", mov_pc);
-        printf("Quantidade de movimentação!\n");
-        scanf("%d", &qnt_mov);
-        break;
-
-    case 2:
-        peca = "Torre";
-        printf("Escolha qual direção da Torre!\nCima\nBaixo\n\n");
-        scanf("%s", mov_pc);
-        printf("Quantidade de movimentação!\n");
-        scanf("%d", &qnt_mov);
-        break;
-
-    case 3:
-        peca = "Rainha";
-        printf("Escolha qual direção da Rainha!\nCima\nBaixo\n\n");
-        scanf("%s", mov_pc);
-        printf("Quantidade de movimentação!\n");
-        scanf("%d", &qnt_mov);
-    default:
-        printf("Escolha entre as três opções!");
-        break;
-    }
-
-        
-    for (i = 0; i <= (qnt_mov-1); i++)
-    {
-        if(escolha_peca == 1){
-            printf("%s foi uma casa para %s!\n", peca, mov_pc);
+    // Cavalo
+    int mov_x = 3;
+    int mov_y = 2;
+    char direcao_CIMA[10] = "Cima\n";
+    char direcao_BAIXO[10] = "Baixo\n";
+    char direcao_DIREITA[10] = "Direita\n";
+    char direcao_ESQUERDA[10] = "Esquerda\n";
+    //INICIO DA PECA
+    int linha = 1;
+    int y = 1;
+    char coluna = 'b';
+    for (linha; linha < mov_x; linha++){
+        while(y < mov_y o){
+            //DIRECAO
+            printf("%s\n", direcao_DIREITA);
+            coluna++;
+            y++;
         }
+        //DIRECAO
+        printf("%s\n", direcao_CIMA);
     }
-
-
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-
-    return 0;
-}
+    printf("Linha %d\nColuna %c\n", linha, coluna);
+}   
